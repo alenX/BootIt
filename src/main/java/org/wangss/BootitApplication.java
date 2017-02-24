4ae9a70c4b7bb6c9e3178b30c71a1e612d9b98d6
wangss <genhaoai@gmail.com>
2017-02-24 15:33:28 +0800

    Initial commit

---
+++ src/main/java/org/wangss/BootitApplication.java
@@ -0,0 +1,12 @@
+package org.wangss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootitApplication.class, args);
+	}
+}
---
+++ src/main/java/org/wangss/Controller/IndexController.java
@@ -0,0 +1,21 @@
+package org.wangss.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+/**
+ * Created by wangss on 2017/2/24.
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping("/index")
+    public String index(Map<String,Object> map){
+
+        map.put("name","FreeMarker");
+        return "/index/index";
+
+    }
+}
