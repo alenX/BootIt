ea35a7f20c211e1317663a58a2dd746aecb3e66a
wangss <genhaoai@gmail.com>
2017-02-27 10:58:41 +0800

    注册页面调整

--- src/main/java/org/wangss/Controller/IndexController.java
+++ src/main/java/org/wangss/Controller/IndexController.java
@@ -2,13 +2,13 @@ package org.wangss.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.wangss.Daos.BootUserDao;
 import org.wangss.Models.BootUser;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
 /**
@@ -29,23 +29,23 @@ public class IndexController {
 
     @ResponseBody
     @RequestMapping("/index/user/{id}")
-    public BootUser getBootUser(Long id) {
+    public BootUser getBootUser(@PathVariable Long id) {
         return bootUserDao.getBootUserByid(id);
     }
 
 
-    @RequestMapping(value = "/index/register",method = RequestMethod.POST)
-    public String register(String username,String password,Map<String,Object> map){
-        BootUser bootUser = new BootUser(username,password);
+    @RequestMapping(value = "/index/register", method = RequestMethod.POST)
+    public String register(String username, String password, Map<String, Object> map) {
+        BootUser bootUser = new BootUser(username, password);
         bootUserDao.save(bootUser);
-        map.put("name",bootUser.getName());
+        map.put("name", bootUser.getName());
         return "/index/succ";
     }
 
     @RequestMapping("/index/login")
-    public String login(String username,String password){
+    public String login(String username, String password) {
         BootUser bootUser = bootUserDao.getBootUserByname(username);
-        if (!bootUser.isPassWord(password)){
+        if (!bootUser.isPassWord(password)) {
             return "/index/error";
         }
 
