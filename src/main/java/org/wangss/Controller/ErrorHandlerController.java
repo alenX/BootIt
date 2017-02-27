b9a4ae7f646f25be555a80654dc8ba0c42fc4102
wangss <genhaoai@gmail.com>
2017-02-27 11:08:25 +0800

    error页面逻辑处理增加

---
+++ src/main/java/org/wangss/Controller/ErrorHandlerController.java
@@ -0,0 +1,21 @@
+package org.wangss.Controller;
+
+import org.springframework.boot.autoconfigure.web.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by wangss on 2017/2/27.
+ */
+@Controller
+public class ErrorHandlerController implements ErrorController {
+    @Override
+    public String getErrorPath() {
+        return "/index/error";
+    }
+
+    @RequestMapping
+    public String getErrPage(){
+        return getErrorPath();
+    }
+}
